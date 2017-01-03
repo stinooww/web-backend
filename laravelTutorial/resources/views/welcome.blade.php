c3c74720bd46713d350b1e03eeb61b62e4dba27d
stijn heynderickx <stijn.heynderickx@gmail.com>
2017-01-03 20:54:45 +0100

    laravel tutorial

--- laravelTutorial/resources/views/welcome.blade.php
+++ laravelTutorial/resources/views/welcome.blade.php
@@ -82,7 +82,13 @@
                     Laravel
                 </div>
 
-               <h1>Hello stinnooooow</h1>
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
             </div>
         </div>
     </body>
